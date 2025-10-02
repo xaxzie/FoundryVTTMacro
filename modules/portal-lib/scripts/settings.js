f8db8da26cc69df55e85da77502b3ccc0f6e4ccc
xaxzie <xaxzie@gmail.com>
2025-10-03 00:54:10 +0200

    feat: Add portal-lib module with core functionality and UI components

---
+++ modules/portal-lib/scripts/settings.js
@@ -0,0 +1,36 @@
+import { MODULE_ID } from "./main.js";
+
+export function registerSettings() {
+    const settings = {
+        "autoDelete": {
+            scope: "world",
+            config: true,
+            type: Boolean,
+            default: false,
+        },
+        /*"enableRouter": {
+            scope: "world",
+            config: true,
+            type: Boolean,
+            default: false,
+        },*/
+    };
+
+    registerSettingsArray(settings);
+}
+
+export function getSetting(key) {
+    return game.settings.get(MODULE_ID, key);
+}
+
+export async function setSetting(key, value) {
+    return await game.settings.set(MODULE_ID, key, value);
+}
+
+function registerSettingsArray(settings) {
+    for (const [key, value] of Object.entries(settings)) {
+        if (!value.name) value.name = `${MODULE_ID}.settings.${key}.name`
+        if (!value.hint) value.hint = `${MODULE_ID}.settings.${key}.hint`
+        game.settings.register(MODULE_ID, key, value);
+    }
+}
